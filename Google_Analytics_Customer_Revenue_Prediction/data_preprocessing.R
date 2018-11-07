2607b3dcf249edbf5bb1dcede7bef787e3ef2274
ghao89 <guanshengrui.hao@gmail.com>
2018-11-06 16:26:40 -0800

    Add the part preprocessing the test data.

--- Google_Analytics_Customer_Revenue_Prediction/data_preprocessing.R
+++ Google_Analytics_Customer_Revenue_Prediction/data_preprocessing.R
@@ -4,10 +4,7 @@ library(jsonlite)
 
 t <- proc.time()
 
-train <- read.csv("Google_Analytics_Customer_Revenue_Prediction//train.csv")
-
-# # Use a subset of train data set to preprocess
-# train <- train[sample(1:nrow(train), 10000), ]
+train <- read.csv("Google_Analytics_Customer_Revenue_Prediction/train.csv")
 
 geoNetwork <- bind_rows(lapply(train$geoNetwork, FUN = function(x) fromJSON(as.character(x))))
 device <- bind_rows(lapply(train$device, FUN = function(x) fromJSON(as.character(x))))
@@ -25,3 +22,29 @@ train <- select(train, -geoNetwork, -device, -totals, -trafficSource)
 train <- bind_cols(train, geoNetwork, device, totals, trafficSource)
 
 proc.time() - t
+
+rm(geoNetwork, totals, trafficSource, device)
+
+
+t <- proc.time()
+
+test <- read.csv("Google_Analytics_Customer_Revenue_Prediction/test.csv")
+
+geoNetwork <- bind_rows(lapply(test$geoNetwork, FUN = function(x) fromJSON(as.character(x))))
+device <- bind_rows(lapply(test$device, FUN = function(x) fromJSON(as.character(x))))
+totals <- bind_rows(lapply(test$totals, FUN = function(x) fromJSON(as.character(x))))
+trafficSource <- bind_rows(lapply(test$trafficSource, FUN = function(x) 
+  as.character(x) %>%
+    fromJSON() %>%
+    purrr::flatten() %>%
+    map_if(is_list, as_tibble) %>%
+    map_if(is_tibble, list) %>%
+    bind_cols()
+))
+
+test <- select(test, -geoNetwork, -device, -totals, -trafficSource)
+test <- bind_cols(test, geoNetwork, device, totals, trafficSource)
+
+proc.time() - t
+
+save(train, test, file = "Google_Analytics_Customer_Revenue_Prediction/preprocessed.Rdata")
