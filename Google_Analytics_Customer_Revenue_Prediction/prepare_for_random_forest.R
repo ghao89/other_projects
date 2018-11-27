4aa96e0b26d2bb466b3342eb396fcb9d77814bc4
ghao89 <guanshengrui.hao@gmail.com>
2018-11-27 14:40:09 -0800

    Minor edit.

--- Google_Analytics_Customer_Revenue_Prediction/prepare_for_random_forest.R
+++ Google_Analytics_Customer_Revenue_Prediction/prepare_for_random_forest.R
@@ -1,41 +1,43 @@
 library(randomForest)
 library(dplyr)
 
-load(file = "Google_Analytics_Customer_Revenue_Prediction/intermediate.Rdata")
+# load(file = "Google_Analytics_Customer_Revenue_Prediction/intermediate.Rdata")
+# 
+# #------------- Deal with date -------------#
+# train_ref_date <- 20170101
+# test_ref_date <- 20180101
+# 
+# train$date <- unlist(lapply(train$date, FUN = function(x) 
+#   as.double(strptime(x, format = "%Y%m%d") - strptime(train_ref_date, format = "%Y%m%d"))))
+# test$date <- unlist(lapply(test$date, FUN = function(x)
+#   as.double(strptime(x, format = "%Y%m%d") - strptime(test_ref_date, format = "%Y%m%d"))))
+# 
+# #------------- Deal with factors -------------#
+# train$y <- as.numeric(train$transactionRevenue != 0)
+# 
+# var_class <- sapply(train, class)
+# var_names_tr <- names(train)
+# var_names_te <- names(test)
+# 
+# factor_names <- NULL
+# 
+# for (i in 1:length(var_class)) {
+#   var <- var_class[i]
+#   var_name <- var_names_tr[i]
+#   if (var != "numeric" && var != "integer") {
+#     agg <- aggregate(train$y, by = list(train[,i]), FUN = function(x) sum(x)/length(x))
+#     colnames(agg) <- c(var_names_tr[i], paste0(var_names_tr[i], "_x"))
+#     train <- left_join(train, agg, by = var_name)
+#     test <- left_join(test, agg, by = var_name)
+#     
+#     if (var_name != "fullVisitorId") {
+#       factor_names <- c(factor_names, var_name)
+#     }
+#     print(var_name)
+#   }
+# }
 
-#------------- Deal with date -------------#
-train_ref_date <- 20170101
-test_ref_date <- 20180101
-
-train$date <- unlist(lapply(train$date, FUN = function(x) 
-  as.double(strptime(x, format = "%Y%m%d") - strptime(train_ref_date, format = "%Y%m%d"))))
-test$date <- unlist(lapply(test$date, FUN = function(x)
-  as.double(strptime(x, format = "%Y%m%d") - strptime(test_ref_date, format = "%Y%m%d"))))
-
-#------------- Deal with factors -------------#
-train$y <- as.numeric(train$transactionRevenue != 0)
-
-var_class <- sapply(train, class)
-var_names_tr <- names(train)
-var_names_te <- names(test)
-
-factor_names <- NULL
-
-for (i in 1:length(var_class)) {
-  var <- var_class[i]
-  var_name <- var_names_tr[i]
-  if (var != "numeric" && var != "integer") {
-    agg <- aggregate(train$y, by = list(train[,i]), FUN = function(x) sum(x)/length(x))
-    colnames(agg) <- c(var_names_tr[i], paste0(var_names_tr[i], "_x"))
-    train <- left_join(train, agg, by = var_name)
-    test <- left_join(test, agg, by = var_name)
-    
-    if (var_name != "fullVisitorId") {
-      factor_names <- c(factor_names, var_name)
-    }
-    print(var_name)
-  }
-}
+load(file = "Google_Analytics_Customer_Revenue_Prediction/prepared.Rdata")
 
 train <- select(train, -factor_names)
 test <- select(test, -factor_names)
@@ -87,6 +89,21 @@ for (j in 1:num_epoch) {
   # rf_all <- randomForest::combine(rf_all, rf_batch)
 
 }
+pred_tr_y <- NULL
+i <- 0
+tse <- 0
+
+while ((i + 1)*batch_size <= nr) {
+  batch_x <- tr_x[i*batch_size + (1:batch_size),]
+  batch_y <- tr_y[i*batch_size + (1:batch_size)]
+  pred_y <- predict(rf_all, batch_x)
+  tse <- tse + (pred_y - batch_y)^2
+  pred_tr_y <- c(pred_tr_y, pred_y)
+  print(i)
+  i <- i + 1
+}
+
+rmse <- sqrt(mean(tse))
 
 #------------
 te_x <- select(test, -y, -fullVisitorId)
